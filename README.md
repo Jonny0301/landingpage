764014f02d8a5d7a841b6bcae8d7633e241f4583
Jonny0301 <stevenperdomo2424@gmail.com>
2024-08-03 02:16:23 -0700

    landingpage

---
+++ README.md
@@ -0,0 +1,25 @@
+# Wonderful Landing Page Template
+
+### <a href="https://wonderfullandingpage.github.io/mylandingpage/">LIVE DEMO</a> 
+
+![](https://github.com/wonderfullandingpage/mylandingpage/blob/master/imgs/01.jpg?raw=true)
+![](https://github.com/wonderfullandingpage/mylandingpage/blob/master/imgs/02.jpg?raw=true)
+
+## Description
+This is a ReactJS based landing page template, fit for a startup company/service with a one page view.All 'visual' data can be easily modified by changing the data.json file. The project fork is from https://github.com/issaafalkattan/React-Landing-Page-Template. Thanks to the author for a great project.
+
+## Make it Yours!
+### 1. Preps
+You will need to have <a href="https://nodejs.org/">Node JS</a> installed on your pc. 
+
+### 2. Clone Files
+After cloning the files, you will have to run ```yarn``` followed by ```yarn start``` in the CLI
+### 3. Add your own data 
+Change the data in the ```data.json``` file as well as add any images to ```public/img/```
+You can also change styles by modifying the ```public/css``` files.
+
+
+## Credits
+##### Free CSS 
+<a href="https://www.free-css.com/assets/files/free-css-templates/preview/page234/interact/">Free-CSS.com </a>
+
---
+++ src/components/gallery.jsx
@@ -0,0 +1,67 @@
+import React, { useState, useCallback } from "react";
+import ImageViewer from "react-simple-image-viewer";
+
+export const Gallery = props => {
+  const [currentImage, setCurrentImage] = useState(0);
+  const [isViewerOpen, setIsViewerOpen] = useState(false);
+
+  const data = [
+    { thumb: "img/portfolio/01-small.jpg", title: "Lorem Ipsum1" },
+    { thumb: "img/portfolio/02-small.jpg", title: "Lorem Ipsum2" },
+    { thumb: "img/portfolio/03-small.jpg", title: "Lorem Ipsum3" },
+    { thumb: "img/portfolio/04-small.jpg", title: "Lorem Ipsum4" },
+    { thumb: "img/portfolio/05-small.jpg", title: "Lorem Ipsum5" },
+    { thumb: "img/portfolio/06-small.jpg", title: "Lorem Ipsum6" },
+    { thumb: "img/portfolio/07-small.jpg", title: "Lorem Ipsum7" },
+    { thumb: "img/portfolio/08-small.jpg", title: "Lorem Ipsum8" },
+    { thumb: "img/portfolio/09-small.jpg", title: "Lorem Ipsum9" }
+  ];
+
+  const images = data.map(obj => obj.thumb.replace("-small", "-large"));
+
+  const openImageViewer = useCallback(index => {
+    setCurrentImage(index);
+    setIsViewerOpen(true);
+  }, []);
+
+  const closeImageViewer = () => {
+    setCurrentImage(0);
+    setIsViewerOpen(false);
+  };
+
+  return (
+    <div id="portfolio" className="text-center">
+      <div className="container">
+        <div className="section-title">
+          <h2>Gallery</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed dapibus leonec.</p>
+        </div>
+        <div className="row">
+          <div className="portfolio-items">
+            {data.map(({ title, thumb }, index) => (
+              <div key={index} onClick={() => openImageViewer(index)} className="col-sm-6 col-md-4 col-lg-4">
+                <div className="portfolio-item cursor">
+                  <div className="hover-bg">
+                    <div className="hover-text">
+                      <h4>{title}</h4>
+                    </div>
+                    <img src={thumb} className="img-responsive" alt="Project Title" />{" "}
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+
+          {isViewerOpen && (
+            <ImageViewer
+              src={images}
+              backgroundStyle={{ zIndex: 99999 }}
+              currentIndex={currentImage}
+              onClose={closeImageViewer}
+            />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
